6c3b907c6afc523e4ad15b64dce0695bec2e0af0
Oscar Godoy <oscar.go916@gmail.com>
2025-01-20 02:07:25 -0600

    Update main.py

--- src/main.py
+++ src/main.py
@@ -3,7 +3,6 @@
 Created on Sun Jan 19 23:15:04 2025
 @author: oscar
 """
-import time
 from worm_funcs import rgb
 from multiprocessing import Pool
 import numpy as np
@@ -13,11 +12,8 @@ if __name__ == '__main__':
     sy, sx = 1200, 2000
     grid = [(n+1,m+1) for n in range(sy) for m in range(sx)]
 
-    start = time.time()
     with Pool(processes=16) as pool:
         records = pool.starmap(rgb, grid)   
-    end = time.time()
-    print(end-start)
     
     img = np.array(records, dtype=np.uint8).reshape(sy, sx, 3)
     img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
